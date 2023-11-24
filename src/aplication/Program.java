a3b605e6bda6e6666b7d03422fe8912e07601c3f
Raysse Cristine <rayssecristine@hotmail.com>
2023-11-23 21:52:14 -0300

    Checkmate logic

--- src/aplication/Program.java
+++ src/aplication/Program.java
@@ -19,7 +19,7 @@ public class Program {
 		ChessMatch chessMatch = new ChessMatch();
 		List<ChessPiece> captured = new ArrayList<>();
 		
-		while(true) {
+		while(!chessMatch.getCheckMate()) {
 			try {
 				UI.clearScreen();
 				UI.printMatch(chessMatch, captured);
@@ -47,7 +47,9 @@ public class Program {
 			catch (InputMismatchException e) {
 				System.out.println(e.getMessage());
 				sc.nextLine();
+			}
+		}
+		UI.clearScreen();
+		UI.printMatch(chessMatch, captured);
 	}
-  }
-}
 }
